826c3f2a071d7d02428f58c71bc29379f0086a67
ruan-cat <1219043956@qq.com>
2025-09-08 01:01:53 +0800

    🎉 init: 初始化基于 @ruan-cat/vitepress-preset-config 的文档配置。

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,26 +1,29 @@
-import { defineConfig } from "vitepress";
+import { setUserConfig, setGenerateSidebar, addChangelog2doc } from "@ruan-cat/vitepress-preset-config/config";
 
-// https://vitepress.dev/reference/site-config
-export default defineConfig({
-	title: "My Awesome Project",
-	description: "A VitePress Site",
-	themeConfig: {
-		// https://vitepress.dev/reference/default-theme-config
-		nav: [
-			{ text: "Home", link: "/" },
-			{ text: "Examples", link: "/markdown-examples" },
-		],
+import { description } from "../../package.json";
 
-		sidebar: [
-			{
-				text: "Examples",
-				items: [
-					{ text: "Markdown Examples", link: "/markdown-examples" },
-					{ text: "Runtime API Examples", link: "/api-examples" },
-				],
-			},
-		],
-
-		socialLinks: [{ icon: "github", link: "https://github.com/vuejs/vitepress" }],
+// 为文档添加自动生成的changelog
+addChangelog2doc({
+	// 设置changelog的目标文件夹
+	target: "./docs",
+	// 设置changelog顶部的yaml数据。通常是排序
+	data: {
+		order: 1000,
+		dir: {
+			order: 1000,
+		},
 	},
 });
+
+const userConfig = setUserConfig({
+	title: "阮喵喵的github star列表",
+	description,
+	themeConfig: {},
+});
+
+// @ts-ignore
+userConfig.themeConfig.sidebar = setGenerateSidebar({
+	documentRootPath: "./docs",
+});
+
+export default userConfig;
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,17 +1,6 @@
-// https://vitepress.dev/guide/custom-theme
-import { h } from "vue";
-import type { Theme } from "vitepress";
-import DefaultTheme from "vitepress/theme";
+import { defineRuancatPresetTheme } from "@ruan-cat/vitepress-preset-config/theme";
+
+// 增加用户自定义样式
 import "./style.css";
 
-export default {
-	extends: DefaultTheme,
-	Layout: () => {
-		return h(DefaultTheme.Layout, null, {
-			// https://vitepress.dev/guide/extending-default-theme#layout-slots
-		});
-	},
-	enhanceApp({ app, router, siteData }) {
-		// ...
-	},
-} satisfies Theme;
+export default defineRuancatPresetTheme();
